c6193293dc07d4fe6fd630c6421a87e529633594
qualityhair <sandro.stefanati@gmail.com>
2025-06-12 17:21:33 +0200

    Update navbar-loader.js
    aggiornato bottone cerca

--- public/js/navbar-loader.js
+++ public/js/navbar-loader.js
@@ -5,7 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(res => res.text())
       .then(html => {
         container.innerHTML = html;
+
+        // Dopo il caricamento, collega gli event listener
+        const btnCerca = container.querySelector("#btnCerca");
+        if (btnCerca) {
+          btnCerca.addEventListener("click", () => {
+            const modal = document.getElementById("modal-cerca-cliente");
+            if (modal) {
+              modal.classList.remove("hidden");
+            } else {
+              console.error("Modale di ricerca cliente non trovata.");
+            }
+          });
+        }
       })
       .catch(err => console.error("Errore nel caricamento della navbar:", err));
   }
 });
+
